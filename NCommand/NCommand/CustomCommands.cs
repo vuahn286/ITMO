b7ed367fe168c8c64f7ff041643ddd5ed9fd7c64
Vũ Lê Đình <80851187+vuahn286@users.noreply.github.com>
2021-03-20 15:56:17 +0300

    Add files via upload

---
+++ NCommand/NCommand/CustomCommands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace NCommand
+{
+    class CustomCommands
+    {
+        static CustomCommands()
+        {
+            var myInputGestures = new InputGestureCollection();
+            myInputGestures.Add(new KeyGesture(Key.L, ModifierKeys.Control));
+            Launch = new RoutedUICommand("Запуск", "Launch", typeof(CustomCommands), myInputGestures);
+        }
+
+        public static RoutedUICommand Launch { get; }
+    }
+}
